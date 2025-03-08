73c20c4d0f1d1ccf13bc2301b6e7b2dd5fefbc26
Gurvink <93204838+Gurvink@users.noreply.github.com>
2025-03-08 13:52:33 +0100

    Update noMovementRandom.dart
    Margin to sides

--- lib/spawnTypes/noMovementRandom.dart
+++ lib/spawnTypes/noMovementRandom.dart
@@ -9,8 +9,8 @@ class NoMovementRandom extends Spawner{
   Face spawnFaces(int faceAmount, Face target) {
     target.isTarget = true;
     target.position = Vector2(
-      Random().nextDouble() * (game.size.x),
-      Random().nextDouble() * (game.size.y),
+      Random().nextDouble() * (game.size.x - 20) + 20,
+      Random().nextDouble() * (game.size.y - 20) + 20,
     );
     for(int i = 0; i < faceAmount; i++) {
       var character = target.character;
@@ -19,8 +19,8 @@ class NoMovementRandom extends Spawner{
       } while(character == target.character);
       var face = Face(character: character);
       face.position = Vector2(
-        Random().nextDouble() * (game.size.x),
-        Random().nextDouble() * (game.size.y),
+        Random().nextDouble() * (game.size.x - 20) + 20,
+        Random().nextDouble() * (game.size.y - 20) + 20,
       );
       game.add(face);
     }
